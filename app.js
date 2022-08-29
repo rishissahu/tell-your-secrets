ed1945fd3f8f74f21ba4ca76dba4a4427ec50f2b
Rishi <rishisahu4456@gmail.com>
2022-08-29 16:25:32 +0530

    Changed Encryption method to md5

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 
-require('dotenv').config()
+// require('dotenv').config()
+// updated to hashing technique
 const express = require("express")
 const bodyParser = require("body-parser")
 const ejs = require("ejs")
 const mongoose = require("mongoose")
 const encrypt = require('mongoose-encryption');
+const md5 = require("md5")
+
 
 const app =express()
 
@@ -21,7 +24,8 @@ const userSchema =new mongoose.Schema({
     password:String
 })
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET ,encryptedFields:['password']} );
+// userSchema.plugin(encrypt, { secret: process.env.SECRET ,encryptedFields:['password']} );
+// this is the old encruption technique used to store the passwords of user
 
 const User = new mongoose.model("User", userSchema)
 
@@ -40,7 +44,7 @@ app.get("/login", function(req, res){
 app.post("/register", function(req, res){
     const newUser= new User({
         email:req.body.username, 
-        password:req.body.password
+        password:md5(req.body.password)
     })
     newUser.save(function(err){
         if(err){
@@ -56,7 +60,7 @@ app.post("/register", function(req, res){
 
 app.post("/login", function(req, res){
     const username = req.body.username
-    const pass =req.body.password
+    const pass =md5(req.body.password)
     
     User.findOne({email:username},function(err, userFound){
         if(err){
